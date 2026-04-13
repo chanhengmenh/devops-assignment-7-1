9ff40762253b7795a47ee652d275a9005bd9e41a
MENH Chanheng <chanhengmenh@gmail.com>
2026-04-13 23:02:44 +0700

    fix tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,8 +2,23 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Get default VPC (safe for labs)
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get subnets in default VPC
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# Security Group (attached properly to VPC)
 resource "aws_security_group" "web_sg" {
-  name = "web_sg"
+  name   = "web_sg"
+  vpc_id = data.aws_vpc.default.id
 
   ingress {
     from_port   = 8000
@@ -27,12 +42,17 @@ resource "aws_security_group" "web_sg" {
   }
 }
 
+# EC2 Instance
 resource "aws_instance" "app" {
   ami           = "ami-0ec10929233384c7f"
   instance_type = "t2.micro"
-  key_name      = "devops-key"   
+  key_name      = "devops-key"
+
+  subnet_id = data.aws_subnets.default.ids[0]
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  associate_public_ip_address = true
 
   tags = {
     Name = "devops-assignment-7"
@@ -41,7 +61,7 @@ resource "aws_instance" "app" {
   user_data = <<-EOF
               #!/bin/bash
               apt update -y
-              apt install docker.io -y
+              apt install -y docker.io
               systemctl start docker
               systemctl enable docker
               usermod -aG docker ubuntu
